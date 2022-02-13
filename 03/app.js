825455aff6f90c621dc345b5807185882c4dbe0c
Igor <ilg2005@mail.ru>
2022-02-13 18:15:45 +0300

    блок 3

--- 03/app.js
+++ 03/app.js
@@ -3,12 +3,27 @@ const App = {
         title: 'Заголовок',
         myHTML: '<h1>Vue 3 App</h1>',
         items: [1, 2, 3, 4, 5, 6, 7],
+        myItem: '',
         person: {
             firstName: 'Vladilen',
             lastName: 'Minin',
             age: 28
         },
     }),
+    methods: {
+        addItem() {
+            this.items.unshift(this.myItem)
+            this.myItem = ''
+        },
+        log(i) {
+            console.log(i)
+        }
+    },
+    computed: {
+        evenItems()  {
+           return this.items.filter(i => i % 2 === 0)
+        }
+    }
 }
 
 Vue.createApp(App).mount('#app')
