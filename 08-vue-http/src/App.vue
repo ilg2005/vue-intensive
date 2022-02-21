3fb653debef2e40da135939b2817d81000c85eff
ilg2005 <igoldfarb10@gmail.com>
2022-02-21 18:53:30 +0300

    axios

--- 08-vue-http/src/App.vue
+++ 08-vue-http/src/App.vue
@@ -8,20 +8,33 @@
       </div>
       <button class="btn primary" :disabled="!name.length">Создать</button>
     </form>
+    <hr>
+    <div class="card">
+      <app-people-list :people="people"
+                       @load="loadPeople"
+      ></app-people-list>
+    </div>
   </div>
 </template>
 
 <script>
+import AppPeopleList from "@/components/AppPeopleList";
+import axios from 'axios'
+
 export default {
+  components: {AppPeopleList},
   data() {
     return {
       name: '',
+      people: [],
+      url: 'https://vue-with-http-c754b-default-rtdb.europe-west1.firebasedatabase.app/people.json',
     }
   },
   methods: {
-   async createPerson() {
-      const url = 'https://vue-with-http-c754b-default-rtdb.europe-west1.firebasedatabase.app/people.json'
-     const response = await fetch(url, {
+
+    async createPerson() {
+
+      const response = await fetch(this.url, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
@@ -31,9 +44,20 @@ export default {
         })
       })
 
-     const firebaseData =await response.json()
-     console.log(firebaseData)
-     this.name = ''
+      const firebaseData = await response.json()
+      console.log(firebaseData)
+      this.name = ''
+
+    },
+    async loadPeople() {
+
+      const {data} = await axios.get(this.url)
+      this.people = Object.keys(data).map(key => ({
+        id: key,
+        ...data[key]
+      }))
+
+      console.log(this.people)
 
     }
   }
---
+++ 08-vue-http/src/components/AppPeopleList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-if="people.length">
+    <div class="card"
+         v-for="person in people"
+         :key="person.id"
+    >
+      <h3>{{ person.firstName }}</h3>
+    </div>
+  </div>
+  <div class="card center" v-else>
+    <h4>Людей пока нет</h4>
+    <button class="btn" @click="$emit('load')">Загрузить список</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppPeopleList",
+  props: ['people'],
+  emits: ['load'],
+}
+</script>
+
+<style scoped>
+
+</style>
