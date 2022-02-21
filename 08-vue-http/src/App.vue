0fed9dda451228da7a96644956225e34e9c1e496
ilg2005 <igoldfarb10@gmail.com>
2022-02-21 09:59:56 +0300

    форма

--- 08-vue-http/src/App.vue
+++ 08-vue-http/src/App.vue
@@ -1,13 +1,28 @@
 <template>
   <div class="container">
-    <div class="card">
+    <form class="card" @submit.prevent="createPerson">
       <h2>Работа с базой данных</h2>
-    </div>
+      <div class="form-control">
+        <label for="name">Введите имя</label>
+        <input type="text" id="name" v-model="name">
+      </div>
+      <button class="btn primary" :disabled="!name.length">Создать</button>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      name: '',
+    }
+  },
+  methods: {
+    createPerson() {
+      console.log('name: ', this.name)
+    }
+  }
 }
 </script>
 
