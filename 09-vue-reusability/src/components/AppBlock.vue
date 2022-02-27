3fbca9902927adbc4a7a946540c2e5e8236fbbb0
ilg2005 <igoldfarb10@gmail.com>
2022-02-27 11:32:56 +0300

    AppAlert, AppBlock

--- 09-vue-reusability/src/components/AppBlock.vue
+++ 09-vue-reusability/src/components/AppBlock.vue
@@ -4,25 +4,20 @@
         v-if="alert"
         text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, repellat."
         title="Внимание!"
-        type="danger"
-        closable
         @close="alert = false "
     ></app-alert>
 
     <div class="card">
       <button class="btn primary" @click="toggleAlert">{{ alert ? 'Скрыть' : 'Показать' }} сообщение</button>
     </div>
-
-    <app-block></app-block>
   </div>
 </template>
 
 <script>
 import AppAlert from "@/components/AppAlert";
-import AppBlock from "@/components/AppBlock";
 
 export default {
-  components: {AppAlert, AppBlock},
+  components: {AppAlert},
   data() {
     return {
       alert: false
