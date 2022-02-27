ea8080545e55691ffad2231d42a89e316721b776
Igor <ilg2005@mail.ru>
2022-02-27 19:55:34 +0300

    миксины и директивы

--- 09-vue-reusability/src/App.vue
+++ 09-vue-reusability/src/App.vue
@@ -1,40 +1,27 @@
 <template>
   <div class="container">
-    <app-alert
-        v-if="alert"
-        text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, repellat."
-        title="Внимание!"
-        type="danger"
-        closable
-        @close="alert = false "
-    ></app-alert>
-
     <div class="card">
-      <button class="btn primary" @click="toggleAlert">{{ alert ? 'Скрыть' : 'Показать' }} сообщение</button>
+      <h2>Разговор про директивы</h2>
+      <div class="form-control">
+        <label for="inp">Активный по умолчанию</label>
+        <input
+            v-focus
+            type="text"
+            id="inp"
+        >
+      </div>
     </div>
-
-    <app-block></app-block>
   </div>
 </template>
 
 <script>
-import AppAlert from "@/components/AppAlert";
-import AppBlock from "@/components/AppBlock";
+
+import focusDirective from "@/focusDirective";
 
 export default {
-  components: {AppAlert, AppBlock},
-  data() {
-    return {
-      alert: false
-    }
-  },
-
-  methods: {
-    toggleAlert() {
-      this.alert = !this.alert
-    }
+  directives: {
+    focus: focusDirective
   }
-
 }
 </script>
 
--- 09-vue-reusability/src/AppWithMixins.vue
+++ 09-vue-reusability/src/AppWithMixins.vue
@@ -4,21 +4,26 @@
         v-if="alert"
         text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, repellat."
         title="Внимание!"
+        type="danger"
+        closable
         @close="alert = false "
     ></app-alert>
 
     <div class="card">
       <button class="btn primary" @click="toggleAlert">{{ alert ? 'Скрыть' : 'Показать' }} сообщение</button>
     </div>
+
+    <app-block></app-block>
   </div>
 </template>
 
 <script>
 import AppAlert from "@/components/AppAlert";
+import AppBlock from "@/components/AppBlock";
 import AlertMixin from "@/AlertMixin";
 
 export default {
-  components: {AppAlert},
+  components: {AppAlert, AppBlock},
   mixins: [AlertMixin]
 
 }
