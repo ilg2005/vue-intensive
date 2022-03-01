2b549f83294c73b6a01484c71b55508fe4d33da6
ilg2005 <igoldfarb10@gmail.com>
2022-03-01 18:10:13 +0300

    кастомные директивы

--- 09-vue-reusability/src/App.vue
+++ 09-vue-reusability/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
-    <div class="card">
-      <h2>Разговор про директивы</h2>
+    <div class="card" v-if="show">
+      <h2 v-color:[type].blink.hover="myColor">Разговор про директивы</h2>
       <div class="form-control">
         <label for="inp">Активный по умолчанию</label>
         <input
@@ -10,6 +10,8 @@
             id="inp"
         >
       </div>
+      <button class="btn primary" @click="myColor = 'darkBlue'">Сменить цвет</button>
+      <button class="btn" @click="type = type === 'color' ? 'backgroundColor' : 'color'">Переключить тип</button>
     </div>
   </div>
 </template>
@@ -17,10 +19,28 @@
 <script>
 
 import focusDirective from "@/focusDirective";
+import colorDirective from "@/ColorDirective";
 
 export default {
   directives: {
-    focus: focusDirective
+    focus: focusDirective,
+    color: colorDirective
+  },
+
+  data() {
+    return {
+      myColor: 'darkred',
+      type: 'color',
+      show: true
+    }
+  },
+
+  mounted() {
+/*
+    setTimeout(() => {
+      this.show = false
+    }, 2000)
+*/
   }
 }
 </script>
