c4ead642972c033802b578be462c9f2af7772d1e
ilg2005 <igoldfarb10@gmail.com>
2022-03-02 10:33:30 +0300

    modal teleport

--- 09-vue-reusability/src/App.vue
+++ 09-vue-reusability/src/App.vue
@@ -2,27 +2,38 @@
   <div class="container">
     <div class="card">
       <h2> {{ $i18n('app.title') }}</h2>
-      <button class="btn" @click="changeLang">{{ $i18n('app.changeBtn')}}</button>
+      <button class="btn" @click="changeLang">{{ $i18n('app.changeBtn') }}</button>
     </div>
     <button class="btn" @click="$alert('Плагин работает!')">Alert</button>
+    <button class="btn primary" @click="modal = true">Открыть модалку</button>
+    <teleport to="#modal">
+      <app-modal
+          v-if="modal"
+          @close-modal="modal = false"
+      ></app-modal>
+    </teleport>
   </div>
 </template>
 
 <script>
+import AppModal from "@/components/AppModal";
+
 export default {
   inject: ['changeI18n'],
+  components: {AppModal},
   data() {
     return {
-      isRussian: true
+      isRussian: true,
+      modal: false
     }
   },
 
   methods: {
     changeLang() {
       let locale = this.isRussian ? 'en' : 'ru'
-        this.changeI18n(locale)
-        this.isRussian = !this.isRussian
-        this.$forceUpdate()
+      this.changeI18n(locale)
+      this.isRussian = !this.isRussian
+      this.$forceUpdate()
     }
   }
 
---
+++ 09-vue-reusability/src/components/AppModal.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="modal-backdrop" @click="$emit('closeModal')"></div>
+  <div class="modal">
+    <h3>Это заголовок модалки</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus atque aut consequuntur facilis libero perspiciatis ullam. Dicta eveniet ipsam unde?</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppModal",
+  emits: ['closeModal']
+}
+</script>
+
+<style scoped>
+
+</style>
