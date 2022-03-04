c8a7112566ecbd666ca4c5ea80b14ef3dad0d07e
ilg2005 <igoldfarb10@gmail.com>
2022-03-04 18:37:19 +0300

    $store

--- 11-vue-vuex/src/App.vue
+++ 11-vue-vuex/src/App.vue
@@ -3,7 +3,7 @@
   <div class="container with-nav">
     <div class="card">
       <h1>Про Vuex</h1>
-      <h2>Счетчик 0</h2>
+      <h2>Счетчик {{ $store.state.counter }}</h2>
     </div>
   </div>
 </template>
