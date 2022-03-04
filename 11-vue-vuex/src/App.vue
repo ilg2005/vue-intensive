a22500716b2a6d3ee8ce16551dbedd99677e9aad
ilg2005 <igoldfarb10@gmail.com>
2022-03-04 16:39:33 +0300

    11-vue-vuex initial commit

---
+++ 11-vue-vuex/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container with-nav">
+    <the-navbar></the-navbar>
+    <div class="card">
+      <h1>Про Vuex</h1>
+      <h2>Счетчик 0</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+import TheNavbar from './TheNavbar'
+export default {
+  components: {TheNavbar}
+}
+</script>
