724289fcdc8b7d895bfcb1ab3d2ed57e89b1e900
ilg2005 <igoldfarb10@gmail.com>
2022-03-06 11:32:39 +0300

    ref, reactive, computed

--- 12-vue-composition/src/App.vue
+++ 12-vue-composition/src/App.vue
@@ -1,19 +1,48 @@
 <template>
   <div class="container">
     <div class="card">
-      <h1>Vue Composition Api</h1>
+      <h1> {{ title }}</h1>
+      <small>data, methods, computed, watch</small>
       <hr>
       <p>Название: <strong>{{ name }}</strong></p>
-      <p>Версия: <strong>{{ version }}</strong></p>
+      <p>Версия: <strong>{{ version }} ({{ doubleVersion }})</strong></p>
 
-      <button class="btn" @click="changeInfo">Изменить</button>
+      <button class="btn" @click="change">Изменить</button>
     </div>
   </div>
 </template>
 
 <script>
+import {ref, reactive, isRef, computed} from "vue";
+
 export default {
-  data() {
+  setup() {
+    const name = ref('VueJS')
+    const version = ref(3)
+    const title = ref('Vue Composition Api')
+
+    const framework = reactive({
+      name: 'VueJS',
+      version: 3
+    })
+
+    const doubleVersion = computed(() => version.value * 2)
+
+    function changeInfo() {
+      name.value = 'Vue JS!'
+      version.value = 4
+    }
+
+    return {
+      title,
+      name,
+      version,
+      framework,
+      doubleVersion,
+      change: changeInfo
+    }
+  }
+  /*data() {
     return {
       name: 'VueJS',
       version: 3
@@ -24,6 +53,6 @@ export default {
       this.name = 'Vue JS!'
       this.version = 4
     }
-  }
+  }*/
 }
 </script>
