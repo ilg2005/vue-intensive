fcb1248ac48da4aa3e9efedb06eafc95bf85c71c
Igor <ilg2005@mail.ru>
2022-03-20 20:25:55 +0300

    UsersView.vue

---
+++ 13-vue-ts/src/components/UserList.vue
@@ -0,0 +1,19 @@
+<template>
+<ul class="list">
+  <li
+    class="list-item"
+    v-for="user in users"
+    :key="user.id"
+  >
+    {{ user.name }}
+  </li>
+</ul>
+</template>
+
+<script setup lang="ts">
+const users = [];
+</script>
+
+<style scoped>
+
+</style>
--- 13-vue-ts/src/views/UsersView.vue
+++ 13-vue-ts/src/views/UsersView.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="card">
     <h1>Пользователи</h1>
+    <UserList/>
   </div>
 </template>
+
+<script setup lang="ts">
+import UserList from '@/components/UserList.vue';
+</script>
