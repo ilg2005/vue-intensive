f653ed9254afd324c66e09edaa7bec7b82952a77
ilg2005 <igoldfarb10@gmail.com>
2022-03-23 08:19:23 +0300

    suspense, error

--- 13-vue-ts/src/views/Users.vue
+++ 13-vue-ts/src/views/Users.vue
@@ -1,16 +1,35 @@
 <template>
   <div class="card">
     <h1>Пользователи</h1>
-    <UsersList/>
+    <p style="color: red;" v-if="error">{{ error }}</p>
+    <suspense v-else>
+      <template #default>
+        <UsersList/>
+      </template>
+      <template #fallback>
+        <p>Loading...</p>
+      </template>
+
+    </suspense>
+
   </div>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue';
+import {defineComponent, onErrorCaptured, ref} from 'vue';
 import UsersList from "@/components/UsersList.vue";
 
 export default defineComponent({
-  components: {UsersList,}
+  components: {UsersList,},
+  setup() {
+    const error = ref<null | unknown>(null);
+
+    onErrorCaptured(e => {
+      error.value = e;
+      return true;
+    })
+    return {error};
+  }
 });
 
 </script>
