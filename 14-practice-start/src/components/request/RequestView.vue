4a24442a9ee81afbbae0c2d1a142f49437396d6c
ilg2005 <igoldfarb10@gmail.com>
2022-04-09 18:37:13 +0300

    страница с видом заявки

--- 14-practice-start/src/components/request/RequestView.vue
+++ 14-practice-start/src/components/request/RequestView.vue
@@ -12,7 +12,20 @@
       <p><strong>ФИО: </strong>{{ request.fullName }}</p>
       <p><strong>Телефон: </strong>{{ request.tel }}</p>
       <p><strong>Сумма: </strong>{{ currency(request.sum) }}</p>
-      <p><strong>Статус: </strong><AppStatus :type="request.state"/></p>
+      <p class="form-control">
+        <label for="state">Статус: </label>
+        <select
+            id="state"
+            name="state"
+            v-model="state"
+        >
+          <option v-for="(value, state) in TextMap"
+                  :key="state"
+                  :value=state>{{ value }}
+          </option>
+        </select>
+      </p>
+      <button class="btn" v-if="state !== request.state">Обновить</button>
     </div>
 
   </app-page>
@@ -23,12 +36,13 @@
 <script setup>
 
 import AppPage from '@/components/ui/AppPage';
-import AppStatus from '@/components/ui/AppStatus';
 import {useRoute} from "vue-router";
 import {useStore} from "vuex";
 import {onBeforeMount, ref} from "vue";
 import AppLoader from "@/components/ui/AppLoader";
 import {currency} from "@/utils/currency";
+import {TextMap} from "@/components/ui/AppStatus";
+
 
 const route = useRoute();
 const store = useStore();
@@ -36,10 +50,12 @@ const id = route.params.id;
 
 const loading = ref(false);
 const request = ref();
+const state = ref();
 
 onBeforeMount(async () => {
   loading.value = true;
   request.value = await store.dispatch('request/getRequest', id);
+  state.value = request.value.state;
   loading.value = false;
 });
 
@@ -51,7 +67,17 @@ onBeforeMount(async () => {
   font-size: 1rem;
 }
 
-.badge {
-  display: inline;
+.form-control {
+  display: flex;
+  justify-content: left;
+}
+
+label {
+  margin-right: 0.5rem;
+  margin-top: 0.5rem;
+}
+
+.btn {
+  margin-top: 1rem;
 }
 </style>
