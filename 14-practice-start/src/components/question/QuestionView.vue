34a429539e607c3b4beae8626855c24cbb114159
ilg2005 <igoldfarb10@gmail.com>
2022-04-09 20:23:51 +0300

    страница "Помощь"

---
+++ 14-practice-start/src/components/question/QuestionView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="question" @click="isOpen = !isOpen">
+    <div class="question-title">{{ question.title }}</div>
+    <div class="question-body" v-if="isOpen">{{ question.text }}</div>
+  </div>
+</template>
+
+<script setup>
+import {defineProps, ref} from "vue";
+
+defineProps(['question']);
+
+const isOpen = ref(false);
+</script>
+
+<style scoped>
+
+</style>
--- 14-practice-start/src/views/HelpView.vue
+++ 14-practice-start/src/views/HelpView.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <app-page title="Помощь">
+    <template #header>
+      <router-link class="btn primary" to="/">Назад</router-link>
+    </template>
+
+    <QuestionView
+        v-for="question in questions"
+        :key="question.title"
+        :question="question"
+    />
+
+  </app-page>
 </template>
+
+<script setup>
+import AppPage from "@/components/ui/AppPage";
+import QuestionView from "@/components/question/QuestionView";
+
+const questions = [
+  {title: 'Что есть в данной системе?', text: 'Тут есть все для того, чтобы смотреть, какие заявки добавлял пользователь'},
+  {title: 'Как тут работает авторизация?', text: 'Все реализовано через Firebase'}
+];
+
+</script>
+
+<style scoped>
+.btn {
+  font-size: 1rem;
+}
+</style>
