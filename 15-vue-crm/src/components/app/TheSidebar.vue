9ce5bb29b82e7eb541adcd128dd82cc3ea0547af
ilg2005 <igoldfarb10@gmail.com>
2022-04-11 11:07:38 +0300

    TheSidebar.vue

---
+++ 15-vue-crm/src/components/app/TheSidebar.vue
@@ -0,0 +1,52 @@
+<template>
+  <ul class="sidenav app-sidenav" :class="{open: state}">
+    <router-link
+        v-for="link in links"
+        :key="link.title"
+        :to="link.url"
+        custom
+        v-slot="{ href, navigate, isActive  }"
+    >
+      <li
+          :class="[isActive && 'active']"
+      >
+        <a :href="href" @click="navigate" class="waves-effect waves-orange pointer">{{ link.title }}</a>
+      </li>
+
+    </router-link>
+  </ul>
+
+</template>
+
+<script setup>
+import {defineProps} from "vue";
+
+defineProps(['state'])
+
+const links = [
+  {
+    title: 'Счет',
+    url: '/',
+  },
+  {
+    title: 'История',
+    url: '/history',
+  },
+  {
+    title: 'Планирование',
+    url: '/planning',
+  },
+  {
+    title: 'Новая запись',
+    url: '/record',
+  },
+  {
+    title: 'Категории',
+    url: '/categories',
+  },
+];
+</script>
+
+<style scoped>
+
+</style>
--- 15-vue-crm/src/layouts/MainLayout.vue
+++ 15-vue-crm/src/layouts/MainLayout.vue
@@ -1,62 +1,11 @@
 <template>
   <div class="app-main-layout">
-    <nav class="navbar orange lighten-1">
-      <div class="nav-wrapper">
-        <div class="navbar-left">
-          <a href="#">
-            <i class="material-icons black-text">dehaze</i>
-          </a>
-          <span class="black-text">12.12.12</span>
-        </div>
 
-        <ul class="right hide-on-small-and-down">
-          <li>
-            <a
-                class="dropdown-trigger black-text"
-                href="#"
-                data-target="dropdown"
-            >
-              USER NAME
-              <i class="material-icons right">arrow_drop_down</i>
-            </a>
+    <TheNavbar @sidebar-state="isOpen = !isOpen"/>
 
-            <ul id='dropdown' class='dropdown-content'>
-              <li>
-                <a href="#" class="black-text">
-                  <i class="material-icons">account_circle</i>Профиль
-                </a>
-              </li>
-              <li class="divider" tabindex="-1"></li>
-              <li>
-                <a href="#" class="black-text">
-                  <i class="material-icons">assignment_return</i>Выйти
-                </a>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <TheSidebar :state="isOpen"/>
 
-    <ul class="sidenav app-sidenav open">
-      <li>
-        <a href="#" class="waves-effect waves-orange pointer">Счет</a>
-      </li>
-      <li>
-        <a href="#" class="waves-effect waves-orange pointer">История</a>
-      </li>
-      <li>
-        <a href="#" class="waves-effect waves-orange pointer">Планирование</a>
-      </li>
-      <li>
-        <a href="#" class="waves-effect waves-orange pointer">Новая запись</a>
-      </li>
-      <li>
-        <a href="#" class="waves-effect waves-orange pointer">Категории</a>
-      </li>
-    </ul>
-
-    <main class="app-content">
+    <main :class="['app-content', {full: !isOpen}]">
       <div class="app-page">
 
         <router-view/>
@@ -73,10 +22,13 @@
 
 </template>
 
-<script>
-export default {
-  name: "MainLayout"
-}
+<script setup>
+import TheNavbar from "@/components/app/TheNavbar";
+import TheSidebar from "@/components/app/TheSidebar";
+import {ref} from "vue";
+
+const isOpen = ref(true);
+
 </script>
 
 <style scoped>
