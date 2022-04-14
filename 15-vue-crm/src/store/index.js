2468be83d212fd6d652ab0bcb3b2635bee4f098c
ilg2005 <igoldfarb10@gmail.com>
2022-04-14 21:00:22 +0300

    обработка ошибок

--- 15-vue-crm/src/store/index.js
+++ 15-vue-crm/src/store/index.js
@@ -2,9 +2,20 @@ import { createStore } from 'vuex';
 import auth from "@/store/auth";
 
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
+  state: {
+    error: null
+  },
+  getters: {
+    ERROR : state => state.error
+  },
+  mutations: {
+    SET_ERROR(state, error) {
+      state.error = error;
+    },
+    CLEAR_ERROR(state) {
+      state.error = null;
+    }
+  },
   actions: {
 
   },
