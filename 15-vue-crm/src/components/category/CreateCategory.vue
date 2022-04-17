044d5db53c69f5664aab1a03e799f4f95144e3c8
Igor <ilg2005@mail.ru>
2022-04-17 18:02:17 +0300

    валидация формы создания категорий

--- 15-vue-crm/src/components/category/CreateCategory.vue
+++ 15-vue-crm/src/components/category/CreateCategory.vue
@@ -5,23 +5,32 @@
         <h4>Создать</h4>
       </div>
 
-      <form>
+      <form @submit.prevent="submitHandler">
         <div class="input-field">
           <input
-              id="name"
+              id="create_category_name"
               type="text"
+              v-model.trim="name"
+              :class="{invalid: nameError && nameMeta.touched}"
+              @blur="nameBlur"
           >
-          <label for="name">Название</label>
-          <span class="helper-text invalid">Введите название</span>
+          <label for="create_category_name">Название</label>
+          <span class="helper-text invalid" v-if="nameError && nameMeta.touched">{{ nameError }}</span>
         </div>
 
         <div class="input-field">
           <input
-              id="limit"
+              id="create_category_limit"
               type="number"
+              :value="MIN"
+              @input="limit = $event.target.value"
+              :class="{invalid: limitError && limitMeta.touched}"
+              @blur="limitBlur"
+
           >
-          <label for="limit">Лимит</label>
-          <span class="helper-text invalid">Минимальная величина</span>
+          <label for="create_category_limit" class="active">Лимит</label>
+          <span class="helper-text invalid" v-if="limitError && limitMeta.touched && limit">{{ limitError }}</span>
+          <span class="helper-text invalid" v-if="limitError && limitMeta.touched && !limit">Не менее {{MIN}} рублей</span>
         </div>
 
         <button class="btn waves-effect waves-light" type="submit">
@@ -34,6 +43,48 @@
 </template>
 
 <script setup>
+import {useForm, useField} from 'vee-validate';
+import * as yup from 'yup';
+import {computed} from "vue";
+import {useStore} from "vuex";
+
+
+const store = useStore();
+const MIN = 500;
+const schema = computed(() => {
+  return yup.object({
+    name: yup
+        .string()
+        .required('Введите имя'),
+    limit: yup
+        .number()
+        .required('Это поле должно быть заполнено')
+        .min(MIN, `Не менее ${MIN} рублей.`),
+  });
+});
+// Create a form context with the validation schema
+const {handleSubmit} = useForm({
+  validationSchema: schema,
+});
+
+// No need to define rules for fields
+const {value: name, errorMessage: nameError, meta: nameMeta, handleBlur: nameBlur} = useField('name');
+const {
+  value: limit,
+  errorMessage: limitError,
+  meta: limitMeta,
+  handleBlur: limitBlur,
+} = useField('limit');
+
+const submitHandler = handleSubmit(async values => {
+  try {
+    await store.dispatch('createCategory', values);
+  } catch (e) {
+    console.log(e.message);
+  }
+
+});
+
 </script>
 
 <style scoped>
