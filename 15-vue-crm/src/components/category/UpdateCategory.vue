b1fd443c72e04631a21cc639c9eccb0a7460525d
ilg2005 <igoldfarb10@gmail.com>
2022-04-21 13:39:18 +0300

    текущее состояние

--- 15-vue-crm/src/components/category/UpdateCategory.vue
+++ 15-vue-crm/src/components/category/UpdateCategory.vue
@@ -8,7 +8,7 @@
       <form>
         <div class="input-field">
           <select ref="select">
-            <option value="" disabled selected>Choose your option</option>
+            <option value="" disabled selected>Выберите категорию</option>
             <option value="1">Option 1</option>
             <option value="2">Option 2</option>
             <option value="3">Option 3</option>
