2f3b6252315a3a38283802e0cdd30a9fe07ef0cc
ilg2005 <igoldfarb10@gmail.com>
2022-04-26 11:23:49 +0300

    вывод категорий на страницу плаирования

--- 15-vue-crm/src/views/PlanningView.vue
+++ 15-vue-crm/src/views/PlanningView.vue
@@ -1,17 +1,23 @@
 <template>
-  <div>
+  <AppLoader v-if="isLoading"/>
+  <p v-else-if="!categories.length">Категорий пока нет.
+    <router-link to="/categories"> Добавить новую категорию ?</router-link>
+  </p>
+  <div v-else>
     <div class="page-title">
       <h3>Планирование</h3>
-      <h4>12 212</h4>
+      <h4>{{ bill }}</h4>
     </div>
 
     <section>
-      <div>
+      <div v-for="category in categories"
+           :key="category.id"
+      >
         <p>
-          <strong>Девушка:</strong>
+          <strong>{{ category.name }}:</strong>
           12 122 из 14 0000
         </p>
-        <div class="progress" >
+        <div class="progress">
           <div
               class="determinate green"
               style="width:40%"
@@ -23,10 +29,30 @@
 
 </template>
 
-<script>
-export default {
-  name: "PlanningView"
-}
+<script setup>
+import {useStore} from "vuex";
+import currencyFilter from "@/utils/currencyFilter";
+import AppLoader from "@/components/app/AppLoader";
+import {onMounted, ref} from "vue";
+import {toast} from "@/utils/toast";
+
+const store = useStore();
+
+const user = store.getters.USER;
+const isLoading = ref(true);
+
+const bill = currencyFilter(user.info.bill, 'RUB');
+const categories = ref();
+
+onMounted(async () => {
+  try {
+    categories.value = await store.dispatch('fetchCategories');
+    isLoading.value = false;
+  } catch (e) {
+    toast(e.message, 1);
+  }
+})
+
 </script>
 
 <style scoped>
