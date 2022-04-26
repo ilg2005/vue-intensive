ef3d9b9e3ca2d2c5da2c17981991553320929785
ilg2005 <igoldfarb10@gmail.com>
2022-04-26 21:08:58 +0300

    защита роутов

--- 15-vue-crm/src/router/index.js
+++ 15-vue-crm/src/router/index.js
@@ -1,12 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import {firebaseConfig} from "../../firebase.config.js";
+import {getAuth} from "firebase/auth";
+import {initializeApp} from "firebase/app";
+
+import {toast} from "@/utils/toast";
+import messages from "@/utils/messages";
+
+
+
+const firebase = initializeApp(firebaseConfig);
+const auth = getAuth(firebase);
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView,
-    meta: {layout: 'main'}
+    meta: {layout: 'main', auth: true}
   },
   {
     path: '/login',
@@ -15,7 +26,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "login" */ '../views/LoginView.vue'),
-    meta: {layout: 'empty'}
+    meta: {layout: 'empty', auth: false}
   },
   {
     path: '/register',
@@ -24,43 +35,43 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "register" */ '../views/RegisterView.vue'),
-    meta: {layout: 'empty'}
+    meta: {layout: 'empty', auth: false}
   },
   {
     path: '/categories',
     name: 'categories',
     component: () => import(/* webpackChunkName: "categories" */ '../views/CategoriesView.vue'),
-    meta: {layout: 'main'}
+    meta: {layout: 'main', auth: true}
   },
   {
     path: '/detail',
     name: 'detail',
     component: () => import(/* webpackChunkName: "detail" */ '../views/DetailRecordView.vue'),
-    meta: {layout: 'main'}
+    meta: {layout: 'main', auth: true}
   },
   {
     path: '/history',
     name: 'history',
     component: () => import(/* webpackChunkName: "history" */ '../views/HistoryView.vue'),
-    meta: {layout: 'main'}
+    meta: {layout: 'main', auth: true}
   },
   {
     path: '/planning',
     name: 'planning',
     component: () => import(/* webpackChunkName: "planning" */ '../views/PlanningView.vue'),
-    meta: {layout: 'main'}
+    meta: {layout: 'main', auth: true}
   },
   {
     path: '/profile',
     name: 'profile',
     component: () => import(/* webpackChunkName: "profile" */ '../views/ProfileView.vue'),
-    meta: {layout: 'main'}
+    meta: {layout: 'main', auth: true}
   },
   {
     path: '/record',
     name: 'record',
     component: () => import(/* webpackChunkName: "record" */ '../views/RecordView.vue'),
-    meta: {layout: 'main'}
+    meta: {layout: 'main', auth: true}
   },
 ]
 
@@ -68,5 +79,17 @@ const router = createRouter({
   history: createWebHistory(),
   routes
 })
+
+router.beforeEach((to, _, next) => {
+  const currentUser = auth.currentUser;
+  const requiresAuth = to.meta.auth;
+  if (requiresAuth && currentUser === null) {
+    next('/login?message=login');
+    toast(messages['login']);
+  } else {
+    next();
+  }
+});
+
 
 export default router
--- 15-vue-crm/src/utils/messages.js
+++ 15-vue-crm/src/utils/messages.js
@@ -1,5 +1,6 @@
 export default {
     'logout': 'Вы вышли из системы',
     'auth/user-not-found': 'Пользователь с таким email не найден',
-    'auth/wrong-password': 'Неверный пароль'
+    'auth/wrong-password': 'Неверный пароль',
+    'login': 'Нужно войти в систему',
 }
