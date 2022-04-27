1728c3fe702d595cfcd7942fad1b3073bf40a352
ilg2005 <igoldfarb10@gmail.com>
2022-04-27 12:48:27 +0300

    HistoryView.vue

---
+++ 15-vue-crm/src/components/history/RecordsTable.vue
@@ -0,0 +1,54 @@
+<template>
+  <table>
+    <thead>
+    <tr>
+      <th>#</th>
+      <th>Сумма</th>
+      <th>Дата</th>
+      <th>Категория</th>
+      <th>Тип</th>
+      <th>Открыть</th>
+    </tr>
+    </thead>
+
+    <tbody>
+    <tr v-for="(record, idx) in records"
+        :key="record.id"
+    >
+      <td>{{ idx + 1 }}</td>
+      <td>{{ currencyFilter(record.amount, 'RUB') }}</td>
+      <td>{{ record.date }}</td>
+      <td>{{ record.category }}</td>
+      <td>
+        <span :class="['white-text', 'badge', record.class ]">{{ record.typeDescr }}</span>
+      </td>
+      <td>
+        <button
+            v-tooltip="'Открыть запись'"
+            class="btn-small btn"
+            @click="$router.push('/detail')"
+        >
+          <i class="material-icons">open_in_new</i>
+        </button>
+      </td>
+    </tr>
+    </tbody>
+  </table>
+
+</template>
+
+<script setup>
+import {defineProps} from "vue";
+import currencyFilter from "@/utils/currencyFilter";
+
+defineProps({
+  records: {
+    type: Array,
+    required: true,
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
--- 15-vue-crm/src/views/HistoryView.vue
+++ 15-vue-crm/src/views/HistoryView.vue
@@ -4,49 +4,57 @@
       <h3>История записей</h3>
     </div>
 
-    <div class="history-chart">
-      <canvas></canvas>
+    <AppLoader v-if="isLoading"/>
+    <p v-else-if="!records.length" class="center">
+      Записей пока нет.
+      <router-link to="/record">Добавить первую?</router-link>
+    </p>
+    <div v-else>
+      <div class="history-chart">
+        <canvas></canvas>
+      </div>
+
+      <section>
+        <RecordsTable :records="records"/>
+      </section>
     </div>
-
-    <section>
-      <table>
-        <thead>
-        <tr>
-          <th>#</th>
-          <th>Сумма</th>
-          <th>Дата</th>
-          <th>Категория</th>
-          <th>Тип</th>
-          <th>Открыть</th>
-        </tr>
-        </thead>
-
-        <tbody>
-        <tr>
-          <td>1</td>
-          <td>1212</td>
-          <td>12.12.32</td>
-          <td>name</td>
-          <td>
-            <span class="white-text badge red">Расход</span>
-          </td>
-          <td>
-            <button class="btn-small btn">
-              <i class="material-icons">open_in_new</i>
-            </button>
-          </td>
-        </tr>
-        </tbody>
-      </table>
-    </section>
   </div>
 
 </template>
 
-<script>
-export default {
-  name: "HistoryView"
-}
+<script setup>
+import RecordsTable from '@/components/history/RecordsTable';
+import AppLoader from "@/components/app/AppLoader";
+import {onMounted, ref, computed} from "vue";
+import {useStore} from "vuex";
+
+
+const store = useStore();
+
+const isLoading = ref(true);
+const records = ref();
+
+
+onMounted(async () => {
+  const recs = await store.dispatch('fetchRecords');
+  const categories = await store.dispatch('fetchCategories');
+  if (recs) {
+    recs.map(record => {
+      record.category = categories.find(category => category.id === record.categoryId).name;
+      record.class = record.type === 'outcome' ? 'red' : 'green';
+      record.typeDescr = record.type === 'outcome' ? 'Расход' : 'Доход';
+      record.date = computed(() => {
+        return new Intl.DateTimeFormat('ru-RU', {
+          year: 'numeric',
+          month: 'numeric',
+          day: 'numeric',
+        }).format(new Date(record.created));
+      });
+    })
+  }
+  records.value = recs ?? [];
+  isLoading.value = false;
+});
 </script>
 
 <style scoped>
